db8c03ccbe1614e889636a259fde9f7f8fc9da60
Deazm <Deazmhelp@gmail.com>
2018-10-27 21:24:23 +0300

    init commit

---
+++ configs/main.php
@@ -0,0 +1,28 @@
+<?php
+$main = array (
+  's_name' => 'WebMCR',
+  's_about' => 'Система управления сайтом',
+  's_keywords' => 'Minecraft, webmcr, сервер, Qexy',
+  's_dpage' => 'news',
+  's_theme' => 'default',
+  'rewrite' => false,
+  'install' => false,
+  'p_logic' => 'usual',
+  'debug' => true,
+  's_lang' => 'ru_RU',
+  's_root' => '/',
+  's_root_full' => 'http://localhost/',
+  'mcr_secury' => '(l#Xga0$lU0zcn6EliH9',
+  'crypt' => 0,
+  'reg_accept' => true,
+  'captcha' => 1,
+  'rc_public' => '6LdNLf8SAAAAABs0jjSANg543acy6NGi4bH1MX-A',
+  'rc_private' => '6LdNLf8SAAAAAO-XQUcGglTRw4ZZw2tO8163hudD',
+  'kc_public' => '78758',
+  'kc_private' => 'nBJjeiTqRAuXZlqwklPHSgtlCj',
+  'skin_path' => 'uploads/skins/',
+  'cloak_path' => 'uploads/cloaks/',
+  'hd_cloaks' => false,
+  's_client' => 'index.php?mode=start',
+);
+?>
